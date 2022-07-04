3b57e5790e1b32e1d64ee0be34a79e586b07e857
Gustavo <luisgustavoc321@gmail.com>
2022-07-04 20:38:14 -0300

    build: Add Token generator to login sessions

---
+++ src/controllers/Token.js
@@ -0,0 +1,31 @@
+import jwt from 'jsonwebtoken';
+import User from '../models/User';
+
+class TokenController {
+  async store(req, res) {
+    if (!req.body) {
+      return res.status(404).send({ message: 'Error creating  token' });
+    }
+
+    const { email = '', password = '' } = req.body;
+    if (!email || !password) {
+      return res.status(404).send({ message: 'No email or password provided' });
+    }
+
+    const user = await User.findOne({ where: { email } });
+    if (!user) {
+      return res.status(404).send({ message: 'User not found' });
+    }
+
+    if (!(await user.passwordIsValid(password))) return res.status(404).send({ message: 'Password is invalid' });
+
+    const { id } = user;
+    const token = jwt.sign({ id }, process.env.TOKEN_SECRET, {
+      expiresIn: process.env.TOKEN_EXPIRATION,
+    });
+
+    return res.json({ message: 'Token gerado com sucesso', token });
+  }
+}
+
+export default new TokenController();

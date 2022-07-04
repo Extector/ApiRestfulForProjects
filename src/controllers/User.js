4219c300ac40cf5c3cd182c12abc1b0a5505b1d6
Gustavo <luisgustavoc321@gmail.com>
2022-07-03 22:39:06 -0300

    build: User controller file with GET, POST, UPDATE and DELETE methods

---
+++ src/controllers/User.js
@@ -0,0 +1,90 @@
+import User from '../models/User';
+
+class UserController {
+  // Create
+  async store(req, res) {
+    try {
+      const newUser = await User.create({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password,
+      });
+      res.json(newUser);
+    } catch (error) {
+      console.log(error);
+      res.status(400).json({ message: error.message });
+    }
+  }
+
+  // Index
+  async index(req, res) {
+    try {
+      const users = await User.findAll(req.params.id);
+      if (!users) return res.status(404).json({ message: 'No users found' });
+      return res.json(users);
+    } catch (error) {
+      return res.status(400).json({ message: error.message });
+    }
+  }
+
+  // Show
+  async show(req, res) {
+    try {
+      if (!req.params.id && typeof req.params.id !== 'number') {
+        return res
+          .status(404)
+          .json({ message: 'ID not provided / ID does not exists' });
+      }
+
+      const user = await User.findByPk(req.params.id);
+      if (!user) return res.status(404).json({ message: 'User not found' });
+
+      return res.json(user);
+    } catch (error) {
+      return res.status(404).json({ message: 'No user found' });
+    }
+  }
+
+  // Update
+  async update(req, res) {
+    try {
+      if (!req.params.id && typeof req.params.id !== 'number') {
+        return res
+          .status(404)
+          .json({ message: 'ID not provided / ID does not exists' });
+      }
+
+      const user = await User.findByPk(req.params.id);
+      if (!user) {
+        return res.status(404).json({ message: ' User does not exist' });
+      }
+      const updatedUser = await user.update(req.body);
+      return res.json(updatedUser);
+    } catch (error) {
+      return res.status(400).json({ message: error.message });
+    }
+  }
+
+  // Delete
+  async delete(req, res) {
+    try {
+      if (!req.params.id && typeof req.params.id !== 'number') {
+        return res
+          .status(404)
+          .json({ message: 'ID not provided / ID does not exists' });
+      }
+
+      const user = await User.findByPk(req.params.id);
+      if (!user) {
+        return res.status(404).json({ message: ' User does not exist' });
+      }
+
+      await user.destroy();
+      return res.status(200).json({ message: 'User has been deleted' });
+    } catch (error) {
+      return res.status(400).json({ message: error.message });
+    }
+  }
+}
+
+export default new UserController();

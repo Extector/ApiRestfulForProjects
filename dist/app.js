feba3e17b7ebeabd95bad1d0f57524fd8df870f8
Gustavo <luisgustavoc321@gmail.com>
2022-07-10 16:44:12 -0300

    Helmet and Cors to project

--- dist/app.js
+++ dist/app.js
@@ -1,5 +1,7 @@
 "use strict";Object.defineProperty(exports, "__esModule", {value: true}); function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _dotenv = require('dotenv'); var _dotenv2 = _interopRequireDefault(_dotenv);
 var _express = require('express'); var _express2 = _interopRequireDefault(_express);
+var _cors = require('cors'); var _cors2 = _interopRequireDefault(_cors);
+var _helmet = require('helmet'); var _helmet2 = _interopRequireDefault(_helmet);
 var _path = require('path');
 
 var _home = require('./routes/home'); var _home2 = _interopRequireDefault(_home);
@@ -21,6 +23,8 @@ class App {
   }
 
   middlewares() {
+    this.app.use(_cors2.default.call(void 0, ));
+    this.app.use(_helmet2.default.call(void 0, ));
     this.app.use(_express2.default.urlencoded({ extended: true }));
     this.app.use(_express2.default.json());
     this.app.use(_express2.default.static(_path.resolve.call(void 0, __dirname, 'uploads')));
--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
 import { resolve } from 'path';
 
 import homeRoutes from './routes/home';
@@ -21,6 +23,8 @@ class App {
   }
 
   middlewares() {
+    this.app.use(cors());
+    this.app.use(helmet());
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json());
     this.app.use(express.static(resolve(__dirname, 'uploads')));
